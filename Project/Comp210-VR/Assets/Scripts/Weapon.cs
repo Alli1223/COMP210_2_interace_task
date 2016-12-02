be87414670ed5fff74190a0607c1660e9969f032
Alastair Rayner <alli0@me.com>
2016-12-02 13:18:42 +0000

    Player CS script added

--- Project/Comp210-VR/Assets/Scripts/Weapon.cs
+++ Project/Comp210-VR/Assets/Scripts/Weapon.cs
@@ -5,25 +5,28 @@ public class Weapon : MonoBehaviour {
 
     public GameObject cube;
     public Rigidbody rb;
+    public Shader materialOne;
 
     // Use this for initialization
     void Start()
     {
         
         rb = this.cube.GetComponent<Rigidbody>();
-        
-
     }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Sphere")
             Destroy(other.gameObject);
+
+
+        materialOne = other.gameObject.GetComponent<Shader>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        //rb.AddForce(1, 0, 1);
+        //rb.AddForce(0, 5, 0);
     }
 }
