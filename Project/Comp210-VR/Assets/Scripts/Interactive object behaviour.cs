9810f2232e2dc5f7853e93a730afc68e00273f4b
Alastair Rayner <alli0@me.com>
2016-12-02 03:42:14 +0000

    Vr Demo upload

---
+++ Project/Comp210-VR/Assets/Scripts/Interactive object behaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactiveobjectbehaviour : MonoBehaviour {
+
+    public GameObject interactiveObject;
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+     }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Activated");
+	}
+
+
+}
