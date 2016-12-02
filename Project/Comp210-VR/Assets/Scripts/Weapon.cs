7db25de514a60eaa14a944f1b8c67f02c2c8f493
Alastair Rayner <alli0@me.com>
2016-12-02 12:38:52 +0000

    Box removes sphere from scene if collided

---
+++ Project/Comp210-VR/Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    public GameObject cube;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        rb = this.cube.GetComponent<Rigidbody>();
+        
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Sphere")
+            Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //rb.AddForce(1, 0, 1);
+    }
+}
